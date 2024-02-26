6ce1ea141f7eb9886dab455a018f9c0cf023aba4
JerrySiRi <15210723175@163.com>
2024-02-26 21:53:11 +0800

    add some comment of Part I

--- HKU_exchange/NLP_COMP3361/Assignment2/a3-distrib/letter_counting.py
+++ HKU_exchange/NLP_COMP3361/Assignment2/a3-distrib/letter_counting.py
@@ -60,21 +60,36 @@ if __name__ == '__main__':
     print(args)
 
     # Constructs the vocabulary: lowercase letters a to z and space
-    vocab = [chr(ord('a') + i) for i in range(0, 26)] + [' ']
+    vocab = [chr(ord('a') + i) for i in range(0, 26)] + [' '] # 27个字符，26个小写字母+空格
+    # tip：此时是先转换成ASCII码，再转成character。不用把a-z显示写出来啦
     vocab_index = Indexer()
     for char in vocab:
         vocab_index.add_and_get_index(char)
+    # 【补】
+    # 在 Python 中要将某一类型的变量或者常量转换为字符串对象通常有两种方法，即str() 或者 repr()
+    # str()和repr()区别：
+    #   函数str( )将其转化成为适于人阅读的前端样式文本
+    #   函数repr(object)将对象转化为供解释器读取的形式。返回一个对象的 string 格式。
     print(repr(vocab_index))
 
     count_only_previous = True if args.task == "BEFORE" else False
 
     # Constructs and labels the data
     train_exs = read_examples(args.train)
-    train_bundles = [LetterCountingExample(l, get_letter_count_output(l, count_only_previous), vocab_index) for l in train_exs]
+    train_bundles = \
+        [LetterCountingExample(l, get_letter_count_output(l, count_only_previous), \
+            vocab_index) for l in train_exs]
     dev_exs = read_examples(args.dev)
-    dev_bundles = [LetterCountingExample(l, get_letter_count_output(l, count_only_previous), vocab_index) for l in dev_exs]
+    dev_bundles = \
+        [LetterCountingExample(l, get_letter_count_output(l, count_only_previous), \
+            vocab_index) for l in dev_exs]
 
     model = train_classifier(args, train_bundles, dev_bundles)
+
+    # 【？】train model的时候，传入dev的信息干什么？
+    # 难道是train、dev的信息都train，之后再只在dev上检验一下fitting的程度？
+    # 难道不是只用train.txt的信息，dev.txt验证fitting程度后，再把两个数据集同时作为training data来train？
+
     # Decodes the first 5 dev examples to display as output
     decode(model, dev_bundles[0:5], do_print=True, do_plot_attn=True)
     # Decodes 100 training examples and the entire dev set (1000 examples)
